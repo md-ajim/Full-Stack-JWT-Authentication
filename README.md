6bde984134704d1fc1c81759a4703037768ee397
md-ajim <azimislam266@gmail.com>
2025-04-29 07:26:28 +0600

    Update .gitignore for fullstack project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Full-Stack-JWT-Authentication_
+
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion} from 'framer-motion'
 
 import { useSession } from "next-auth/react";
 import Navbar02Page from "@/components/layout/navbar/navbar";
@@ -13,6 +14,13 @@ export default function Home() {
       <Navbar02Page />
       <div className=" max-w-screen-xl mx-auto px-4 md:px-0 h-screen">
         <h1 className=" text-red-800  text-2xl"> This is a home page </h1>
+        <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                className="px-8 py-3 rounded-full dark:bg-white/10  text-white font-medium border  bg-linear-to-r from-indigo-500  to-purple-500 border-white/20 "
+              >
+                View My Work
+              </motion.button>
       </div>
       <Footer04Page />
     </>
